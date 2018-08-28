34dcf09d70cdda3fd8763778eae78de8d84f7d12
Jerry Chou <jjery2243542@gmail.com>
2018-08-28 16:27:11 +0800

    OK to load data

---
+++ dataloader.py
@@ -0,0 +1,69 @@
+import numpy as np
+from torch.utils.data import DataLoader
+import torch
+from dataset import PartedDataset
+
+def _collate_fn(l):
+    l.sort(key=lambda x: x[0].shape[0], reverse=True)
+    features = [torch.from_numpy(feature) for feature, _ in l]
+    padded_features = torch.nn.utils.rnn.pad_sequence(features, batch_first=True, padding_value=0)
+    ilens = [feature.shape[0] for feature, _ in l]
+    texts = [torch.from_numpy(text) for _, text in l]
+    return padded_features, ilens, texts
+
+class PartLoaderIter:
+    def __init__(self, loader):
+        assert len(loader) > 0
+        self.loader = loader
+        self.shuffle = loader.shuffle
+        self.batch_size = loader.batch_size
+        self.in_iter = iter(loader.in_loader)
+        self.part_iter = self.get_part_iter(next(self.in_iter)[0])
+
+    def get_part_iter(self, part):
+        return iter(DataLoader(part,
+                               batch_size=self.batch_size,
+                               shuffle=self.shuffle,
+                               collate_fn=_collate_fn,
+                               num_workers=4))
+
+    def __len__(self):
+        return self.loader._len
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            ret = next(self.part_iter)
+        except StopIteration:
+            self.part_iter = self.get_part_iter(next(self.in_iter)[0])
+            ret = next(self.part_iter)
+        return ret
+
+class PartLoader:
+    def __init__(self, dataset, batch_size=1, shuffle=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.in_loader = DataLoader(dataset, batch_size=1, shuffle=shuffle,
+                                    num_workers=1, collate_fn=lambda x: x)
+
+        self._len = self.dataset.batch_count(self.batch_size)
+
+    def __len__(self):
+        return self._len
+
+    def __iter__(self):
+        return PartLoaderIter(self)
+
+if __name__ == '__main__':
+    config = {'max_feature_length': 1600,
+            'min_feature_length': 30, 
+            'max_text_length': 100,
+            'min_text_length': 2}
+    ds = PartedDataset('/storage/feature/LibriSpeech/packed/train-clean-360/', config=config)
+    data_loader = PartLoader(ds, batch_size=32, shuffle=False)
+    print(len(data_loader))
+    for data in data_loader:
+        print(data[0].size(), len(data[1]), len(data[2]), data[2][0].size())
---
+++ dataset.py
@@ -0,0 +1,73 @@
+import torch 
+from torch.utils.data import Dataset
+import os
+import pickle 
+import glob
+
+class PickleDataset(Dataset):
+    def __init__(self, pickle_path, keys):
+        with open(pickle_path, 'rb') as f:
+            self.data_dict = pickle.load(f)
+        self.keys = keys
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, index):
+        utt_id = self.keys[index]
+        features = self.data_dict[utt_id]['fbank']
+        text = self.data_dict[utt_id]['bpe']
+        return features, text
+
+class PartedDataset(Dataset):
+    def __init__(self, root_dir, config):
+        self.root_dir = os.path.expanduser(root_dir)
+        self.pkl_pathes = sorted([path for path in glob.glob(os.path.join(self.root_dir, '*.pkl'))])
+        self.part_keys = self.get_keys(config)
+
+    def get_keys(self, config):
+        # return utterance id with length within constraint
+        max_feature_length = config['max_feature_length']
+        min_feature_length = config['min_feature_length']
+        max_text_length = config['max_text_length']
+        min_text_length = config['min_text_length']
+
+        part_keys = []
+        for pkl_path in self.pkl_pathes:
+            with open(pkl_path, 'rb') as f:
+                data_dict = pickle.load(f)
+            keys = [key for key in data_dict 
+                    if data_dict[key]['fbank'].shape[0] <= max_feature_length and 
+                    data_dict[key]['fbank'].shape[0] >= min_feature_length and 
+                    data_dict[key]['bpe'].shape[0] <= max_text_length and 
+                    data_dict[key]['bpe'].shape[0] >= min_text_length]
+            #del data_dict
+            part_keys.append(keys)
+        return part_keys 
+
+    def __len__(self):
+        return len(self.pkl_pathes)
+
+    def __getitem__(self, index):
+        return PickleDataset(self.pkl_pathes[index], self.part_keys[index])
+
+    def batch_count(self, batch_size):
+        batches = 0
+        for keys in self.part_keys:
+            batches += (len(keys) + batch_size - 1) // batch_size
+        return batches
+
+if __name__ == '__main__':
+    config = {'max_feature_length': 1600,
+            'min_feature_length': 30, 
+            'max_text_length': 100,
+            'min_text_length': 2}
+    ds = PartedDataset('/storage/feature/LibriSpeech/packed/train-clean-100/', config=config)
+    part = ds[0]
+    print(ds.batch_count(32))
+    print(len(ds))
+    print(len(part))
+    features, text = part[0]
+    print(features.shape, text.shape, type(features), type(text))
+    
+
