c1c8646783da16646644a54a6b35d93266bd1f01
Jerry Chou <jjery2243542@gmail.com>
2018-11-16 14:27:32 +0800

    turn to batch removing <eos>

--- solver.py
+++ solver.py
@@ -175,7 +175,7 @@ class Solver(object):
 
     def ind2sent(self, all_prediction, all_ys):
         # remove eos and pad
-        prediction_til_eos = remove_pad_eos(all_prediction, eos=self.vocab['<EOS>'])
+        prediction_til_eos = remove_pad_eos_batch(all_prediction, eos=self.vocab['<EOS>'])
         #prediction_til_eos = all_prediction
 
         # indexes to characters
